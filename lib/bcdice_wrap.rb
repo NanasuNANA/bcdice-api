0014093ede64b66b661da6d34109d0d5c8353940
Nanasu NANA <nnanasu7@gmail.com>
2017-11-24 02:04:51 +0900

    BCDiceのバージョンを $bcDiceVersion から取得。

--- lib/bcdice_wrap.rb
+++ lib/bcdice_wrap.rb
@@ -4,7 +4,7 @@ require "diceBot/DiceBot"
 require "diceBot/DiceBotLoader"
 
 class BCDice
-  VERSION = "2.02.72.b1"
+  VERSION = $bcDiceVersion
 
   DICEBOTS = (DiceBotLoader.collectDiceBots + [DiceBot.new]).
     map { |diceBot| [diceBot.gameType, diceBot] }.
